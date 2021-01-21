8d635f4ad4202b61c6f4faebc57f804bba6e50d1
Andres Perez <AndresPerez327@gmail.com>
2021-01-21 06:29:56 -0500

    Added comic  book detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,16 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comics book controller!");
-
-
+            return View();
         }        
        
     }
